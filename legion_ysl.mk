6892ab7e3830909d006af18850eb314d455384b0
Dinesh Kumar <dseera6@gmail.com>
2020-08-05 12:49:06 +0530

    ysl : GApps Added

--- legion_ysl.mk
+++ legion_ysl.mk
@@ -43,3 +43,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
+# Use Gapps
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+endif
