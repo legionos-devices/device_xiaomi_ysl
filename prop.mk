2b493a712eaea5ef84b8c51072c466767704e361
Dinesh Kumar <dseera6@gmail.com>
2020-08-18 19:09:01 +0530

    ysl : Added Maintainer and CPU info

--- prop.mk
+++ prop.mk
@@ -14,3 +14,11 @@ vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam
 vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.maintainer.name=DinEsH(TokYo)
+
+# Cpu
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.processor.model=Snapdragon625
