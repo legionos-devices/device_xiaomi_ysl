4275b113c9ca401b546678af4dac64ed73e38a88
Lucas Dupin <dupin@google.com>
2021-06-06 22:57:10 +0200

    ysl: Kang walleye offsets for GPU composition
    Window animations now have rounded corners, meaning that
    the shader that we use is more expensive than it used to be.
    
    Because of this, we need to adjust the SF offsets, to make sure
    that we'll have enough time to finish the GPU composition without
    dropping frames.
    
    These offsets as similar to blueline's, adjusted slightly because
    we were getting false "frame missed" signals if composition
    starts too early.

--- prop.mk
+++ prop.mk
@@ -260,10 +260,7 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.rcs.supported=1
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.protected_contents=true
-    
+   
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true \
@@ -304,4 +301,15 @@ persist.sys.wfd.virtual=0
 ro.zram.mark_idle_delay_mins=60 
 ro.zram.first_wb_delay_mins=180
 ro.zram.periodic_wb_delay_hours=24
+vendor.qc2.use.bqpool=1
 
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.protected_contents=true
