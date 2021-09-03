aa8fc2bb11a2fe035be1f65821b57212205989e6
MIUI56 <payush706@gmail.com>
2021-09-03 12:22:41 +0000

    ysl:sepolicy: Define sysfs_disk_stat add PRODUCT_BUILD_PROP_OVERRIDES

--- kangos_ysl.mk
+++ kangos_ysl.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
-# Inherit some common Octavi stuff.
+# Inherit some common Kangos stuff.
 
 $(call inherit-product, vendor/kangos/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -29,6 +29,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 USE_GAPPS := true
 TARGET_OPLAUNCHER := true
 KANGOS_BUILDTYPE := OFFICIAL
+
 PRODUCT_PRODUCT_PROPERTIES += \
   ro.kangos.maintainer=AJ \
     ro.kangos.cpu=msm8953
@@ -52,7 +53,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
  BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" 
-
-
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
